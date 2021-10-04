ccc2b4d0c78660fa962e87d48758e266665d5006
TheBunnyMan123 <69465699+TheBunnyMan123@users.noreply.github.com>
2021-10-03 19:38:54 -0500

    Create start.sh

---
+++ minecraft/start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+[ !  -d "/minecraft" ] && mkdir /minecraft
+cd /minecraft
+wget https://papermc.io/api/v2/projects/paper/versions/1.17.1/builds/301/downloads/paper-1.17.1-301.jar -O server.jar
+java -jar -Xmx4096M -Xms4096M server.jar nogui
