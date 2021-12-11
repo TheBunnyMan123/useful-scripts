8d112f08eb0fc7aaf93ee29acd1257c43b4c099f
TheBunnyMan123 <69465699+TheBunnyMan123@users.noreply.github.com>
2021-12-10 21:13:17 -0600

    Update start.sh

--- minecraft/start.sh
+++ minecraft/start.sh
@@ -2,5 +2,5 @@
 
 [ !  -d "/minecraft" ] && mkdir /minecraft
 cd /minecraft
-wget https://papermc.io/api/v2/projects/paper/versions/1.17.1/builds/334/downloads/paper-1.17.1-334.jar -O server.jar
+wget https://papermc.io/api/v2/projects/paper/versions/1.17.1/builds/399/downloads/paper-1.17.1-399.jar -O server.jar
 java -jar -Xmx4096M -Xms4096M server.jar nogui
